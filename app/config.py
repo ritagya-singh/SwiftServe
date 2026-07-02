f6376f0b1c5c4ab3e7b06d4f5259fe9465226c37
ritagya-singh <ritagyasingh10@gmail.com>
2026-07-02 11:42:30 +0530

    Prepare project for Render deployment

--- app/config.py
+++ app/config.py
@@ -26,3 +26,9 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+DATABASE_URL = settings.DATABASE_URL
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(
     autocommit=False,
